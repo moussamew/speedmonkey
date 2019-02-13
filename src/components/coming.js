710c795d547f2a9445aaf99cf06b325cee4b2f47
Unknown <moussa.iskounene@gmail.com>
2019-02-13 16:31:38 +0100

    ADD : Component Coming

---
+++ src/components/coming.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Coming extends Component {
+  render() {
+    return(
+      <div>
+        <p>Bonjour.</p>
+      </div>
+    )
+  }
+}
+
+export default Coming;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/scss/index.scss';
-import App from './components/app';
+import Coming from './components/coming';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Coming />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
