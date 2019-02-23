039eb7291ec6de82dffcbb9084d9ddd51c3b72e0
Unknown <moussa.iskounene@gmail.com>
2019-02-23 02:30:24 +0100

    ADD : Loader / EDIT : Component Coming

--- src/components/coming.js
+++ src/components/coming.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import '../assets/scss/coming.scss';
-import Speedy from "../assets/img/logo.svg";
+import '../assets/scss/loader.scss';
+import Speedy from '../assets/img/logo.svg';
+import btnMail from '../assets/img/mail.svg';
 import { Row, Input, Col, Button } from 'react-materialize';
 import { database } from '../config/firebase';
 import * as firebase from 'firebase';
+
 
 class Coming extends Component {
   constructor(props) {
     super(props);
     this.state = {
       mail: '',
-      error: ''
+      message: '',
+      nbMail: ''
     }
+  }
+
+  componentDidMount(){
+    // Fonction qui récupère le nombre d'entrées en base de données
+    firebase.database().ref().child("mails").orderByChild("mail").once("value").then((snapshot) => {
+      var nbMail = snapshot.numChildren();
+      this.setState({
+        nbMail: nbMail
+      })
+    })
   }
 
   updateMail = (e) => {
     this.setState({
       mail: e.target.value,
-      error: ''
+      message: ''
     })
   }
 
@@ -38,7 +52,7 @@ class Coming extends Component {
         }
       });
 
-      // Créer un message si l'adresse mail existe en base de données ou non
+      // Afficher message d'erreur ou de succes si l'adresse mail existe en base de données ou non
       if(mailExist) {
         this.setState({
           message: 'mailExist'
@@ -54,8 +68,22 @@ class Coming extends Component {
         message: 'error'
       })
     }
-}
+  }
+
   render() {
+    var mailUsers;
+
+    if (this.state.nbMail === '') {
+      mailUsers =
+      <div className="speed-loader">
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
+      </div>;
+    } else {
+      mailUsers = <p>Participants : {this.state.nbMail} / 100 utilisateurs.</p>;
+    }
+
     return(
       <div className="coming-soon">
         <img src={Speedy} alt="Coming soon"/>
@@ -65,7 +93,10 @@ class Coming extends Component {
           <Col s={2}></Col>
           <Input s={8} onChange={this.updateMail} label="Adresse e-mail" />
         </Row>
-        <Button waves='light' onClick={this.handleMail}>S'abonner</Button>
+        <Row className="row-mail">
+          {mailUsers}
+          <Button s={10} waves='light' onClick={this.handleMail}>S'abonner</Button>
+        </Row>
         <div className={this.state.message  === 'error' ? 'error-mail' : 'hidden'}>
             <p>Votre adresse mail n'est pas valide.</p>
         </div>
@@ -75,6 +106,12 @@ class Coming extends Component {
         <div className={this.state.message === 'success' ? 'success-mail' : 'hidden'}>
             <p>Votre adresse mail <strong>{this.state.mail}</strong> a bien été enregistré, nous vous contacterons dès l'ouverture du site.</p>
         </div>
+        <div className="btn-mail">
+          <a href="mailto:hello@speedmonkey.fr">
+      			<img src={btnMail} alt="Envoyez moi un mail"/>
+      		</a>
+        </div>
+
       </div>
     )
   }
