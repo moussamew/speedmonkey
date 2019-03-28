8c559954ca5e46b6cf675bbeb17e4f86ab1ec5b4
Unknown <moussa.iskounene@gmail.com>
2019-03-28 21:36:48 +0100

    ADD : Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,15 @@ import Logo from '../assets/img/logo.svg';
 class Header extends Component {
   render() {
     return (
-      <div className="header">
-        <img src={Logo} alt="SpeedMonkey" className="logo"/>
+      <div>
+        <div className="header">
+          <img src={Logo} alt="SpeedMonkey" className="logo"/>
+          <div className="navigation">
+            <h1>Réalisations</h1>
+            <h1>Explorer le site</h1>
+            <button>Simuler son devis</button>
+          </div>
+        </div>
       </div>
     );
   }
