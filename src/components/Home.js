8eb41689f5af192d578c8861e3a74a3f79323755
Unknown <moussa.iskounene@gmail.com>
2019-03-31 05:11:31 +0200

    EDIT : Home with sections [1]

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,150 +1,78 @@
 import React, { Component } from 'react';
 import '../assets/scss/home.scss';
-import HomeWeb from '../assets/img/home_web.svg';
-import Shopify from '../assets/img/technology/shopify.svg';
-import Reactjs from '../assets/img/technology/react.svg';
-import WordPress from '../assets/img/technology/wordpress.svg';
-import Vuejs from '../assets/img/technology/vue.svg';
-import PrestaShop from '../assets/img/technology/prestashop.svg';
-import Angular from '../assets/img/technology/angular.svg';
-import Drupal from '../assets/img/technology/drupal.svg';
-import Symfony from '../assets/img/technology/symfony.svg';
+import Web from '../assets/img/illustrations/web.svg'; // Illustration n°1 //
+import Creation from '../assets/img/illustrations/creation.svg'; // Illustration n°2 //
+import Referencement from '../assets/img/illustrations/referencement.svg'; // Illustration n°3 //
+import Hebergement from '../assets/img/illustrations/hebergement.svg'; // Illustration n°4 //
+import Quote from '../assets/img/quote.svg'
+import Technology from './Home/Technology' // Liste des plus récentes technologies sous forme d'icones //
 
 class Home extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      shopify: false,
-      react: false,
-      wordpress: false,
-      vue: false,
-      prestashop: false,
-      angular: false,
-      symfony: false
-    }
-
-    this.hoverShopify = this.hoverShopify.bind(this);
-    this.hoverReact = this.hoverReact.bind(this);
-    this.hoverWordpress = this.hoverWordpress.bind(this);
-    this.hoverVue = this.hoverVue.bind(this);
-    this.hoverPrestashop = this.hoverPrestashop.bind(this);
-    this.hoverAngular = this.hoverAngular.bind(this);
-    this.hoverDrupal = this.hoverDrupal.bind(this);
-    this.hoverSymfony = this.hoverSymfony.bind(this);
-
-  }
-
-  hoverShopify(){
-      this.setState({
-        shopify: !this.state.shopify
-      })
-  }
-
-  hoverReact(){
-      this.setState({
-        react: !this.state.react
-      })
-  }
-
-  hoverWordpress(){
-      this.setState({
-        wordpress: !this.state.wordpress
-      })
-  }
-
-  hoverVue(){
-      this.setState({
-        vue: !this.state.vue
-      })
-  }
-
-  hoverPrestashop(){
-      this.setState({
-        prestashop: !this.state.prestashop
-      })
-  }
-
-  hoverAngular(){
-      this.setState({
-        angular: !this.state.angular
-      })
-  }
-
-  hoverDrupal(){
-      this.setState({
-        drupal: !this.state.drupal
-      })
-  }
-
-  hoverSymfony(){
-      this.setState({
-        symfony: !this.state.symfony
-      })
-  }
 
   render() {
-    const shopifySelected = this.state.shopify ? "selected shopify col s1 offset-s2" : "shopify col s1 offset-s2";
-    const reactSelected = this.state.react ? "selected react col s1" : "react col s1";
-    const wordpressSelected = this.state.wordpress ? "selected wordpress col s1" : "wordpress col s1";
-    const vueSelected = this.state.vue ? "selected vue col s1" : "vue col s1";
-    const prestashopSelected = this.state.prestashop ? "selected prestashop col s1" : "prestashop col s1";
-    const angularSelected = this.state.angular ? "selected angular col s1" : "angular col s1";
-    const drupalSelected = this.state.drupal ? "selected drupal col s1" : "drupal col s1";
-    const symfonySelected = this.state.symfony ? "selected symfony col s1" : "symfony col s1";
+
     return (
       <div>
         <div className="row home">
            <div className="col s12 m5 offset-m1">
-            <h1>Concevez, référencez et hebergez votre site web avec SpeedMonkey</h1>
-            <p className="description">Chaque projet web est unique, et mérite le meilleur. Chez Speedmonkey, nous vous proposons une solution technique sur-mesure adapté à votre besoin.</p>
-            <div className="center">
-              <a href="/" className="button btn-home hvr-buzz">Profitez de nos avantages</a>
-              <p className="support">Gratuit. Support 24/7.</p>
-            </div>
+              <h1>Concevez, référencez et hebergez votre site web avec SpeedMonkey</h1>
+              <p className="description">Chaque projet web est unique, et mérite le meilleur.
+              Chez Speedmonkey, nous vous proposons une solution technique sur-mesure adapté à votre besoin.</p>
+              <div className="center">
+                <a href="/" className="button btn-home hvr-buzz">Profitez de nos avantages</a>
+                <p className="support">Gratuit. Support 24/7.</p>
+              </div>
            </div>
-           <div className="col s12 m5 center">
-            <img src={HomeWeb} alt="WebSite" className="website"/>
+           <div className="col s12 m5 center mt-25">
+              <img src={Web} alt="Avec Speedmonkey, vous êtes entre de bonnes mains." className="illustration"/>
            </div>
         </div>
-        <div className="row">
-          <div className="technology center">
-            <p>Nous maîtrisons les plus récentes technologies pour répondre toujours plus efficacement à vos besoins.</p>
-            <div className={shopifySelected}>
-              <img src={Shopify} alt="Shopify" className="shopify hvr-pulse" onMouseEnter={this.hoverShopify} onMouseLeave={this.hoverShopify}/>
-              <p>Shopify</p>
-            </div>
-            <div className={reactSelected}>
-              <img src={Reactjs} alt="React.js" className="react hvr-pulse" onMouseEnter={this.hoverReact} onMouseLeave={this.hoverReact}/>
-              <p>React.js</p>
-            </div>
-            <div className={wordpressSelected}>
-              <img src={WordPress} alt="WordPress" className="wordpress hvr-pulse" onMouseEnter={this.hoverWordpress} onMouseLeave={this.hoverWordpress}/>
-              <p>WordPress</p>
-            </div>
-            <div className={vueSelected}>
-              <img src={Vuejs} alt="Vue.js" className="vue hvr-pulse" onMouseEnter={this.hoverVue} onMouseLeave={this.hoverVue}/>
-              <p>Vue.js</p>
-            </div>
-            <div className={prestashopSelected}>
-              <img src={PrestaShop} alt="PrestaShop" className="prestashop hvr-pulse" onMouseEnter={this.hoverPrestashop} onMouseLeave={this.hoverPrestashop}/>
-              <p>PrestaShop</p>
-            </div>
-            <div className={angularSelected}>
-              <img src={Angular} alt="PrestaShop" className="angular hvr-pulse" onMouseEnter={this.hoverAngular} onMouseLeave={this.hoverAngular}/>
-              <p>Angular</p>
-            </div>
-            <div className={drupalSelected}>
-              <img src={Drupal} alt="Drupal" className="drupal hvr-pulse" onMouseEnter={this.hoverDrupal} onMouseLeave={this.hoverDrupal}/>
-              <p>Drupal</p>
-            </div>
-            <div className={symfonySelected}>
-              <img src={Symfony} alt="Drupal" className="symfony hvr-pulse" onMouseEnter={this.hoverSymfony} onMouseLeave={this.hoverSymfony}/>
-              <p>Symfony</p>
-            </div>
+        <Technology />
+        <div className="row home bg-blue mt100">
+          <div className="col s12 m5 offset-m1 center mt55 mb100">
+              <img src={Creation} alt="Création et refonte de site web" className="illustration"/>
+          </div>
+          <div className="col s12 m5 mt80">
+            <img alt="Quote" src={Quote} />
+            <h2>Nous concrétisons votre projet de <br /><span className="span-red">création</span> ou de <span className="span-red">refonte</span> de site web</h2>
+            <p className="description">Chaque projet web est unique, et mérite le meilleur.
+            Chez Speedmonkey, nous vous proposons une solution technique sur-mesure adapté à votre besoin.</p>
+            <a href="/" className="button btn-section btn-red hvr-icon-push">Réaliser son site web
+              <i className="material-icons hvr-icon icon">code</i>
+            </a>
+          </div>
+        </div>
+        <div className="row home mt20">
+          <div className="col s12 m5 offset-m1 mt60">
+            <img alt="Quote" src={Quote} />
+            <h2>Nous renforçons efficacement le <br/><span className="span-purple">référencement</span> de votre site web</h2>
+            <p className="description">Chaque projet web est unique, et mérite le meilleur.
+            Chez Speedmonkey, nous vous proposons une solution technique sur-mesure adapté à votre besoin.</p>
+            <a href="/" className="button btn-section btn-purple hvr-icon-spin">Mieux référencer son site web
+              <i className="material-icons hvr-icon icon">cached</i>
+            </a>
+          </div>
+          <div className="col s12 m5 center mt35">
+              <img src={Referencement} alt="Référencement de site web" className="illustration"/>
+          </div>
+        </div>
+        <div className="row home bg-blue mt100">
+          <div className="col s12 m5 offset-m1 center mt55 mb100">
+              <img src={Hebergement} alt="Hébergement de site web" className="illustration"/>
+          </div>
+          <div className="col s12 m5 mt80">
+            <img alt="Quote" src={Quote} />
+            <h2>Nous vous proposons un <span className="span-green">hébergement</span> <br/>de qualité pour votre site web</h2>
+            <p className="description">Chaque projet web est unique, et mérite le meilleur.
+            Chez Speedmonkey, nous vous proposons une solution technique sur-mesure adapté à votre besoin.</p>
+            <a href="/" className="button btn-section btn-green hvr-icon-buzz">Héberger son site web
+              <i className="material-icons hvr-icon icon">cloud_circle</i>
+            </a>
           </div>
         </div>
       </div>
     )
+
   }
 }
 
